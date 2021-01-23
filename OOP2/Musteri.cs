5fc1ffd325c93a63998301c14f6109e81809a954
SultanTarak <begumsultant@gmail.com>
2021-01-23 23:56:31 +0300

    23 Ocak Ders Sonu

---
+++ OOP2/Musteri.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP2
+{
+    class Musteri
+    {
+
+        public int Id { get; set; }
+        public string MusteriNo { get; set; }
+
+        //Bir nesnede bir değeri kullanmak zorunda gibi görünmüyorsan demek ki orada soyutlama hatası yapıyorsun
+
+    }
+}
